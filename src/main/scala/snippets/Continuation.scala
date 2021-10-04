515164093a96ffa2f2566e1c4f5cf4f8461cc408
Michael Nikolich <michael.nikolich@googlemail.com>
2021-10-04 08:05:33 +0100

    Add an example for continuation of a second program following the interruption of a first program.

---
+++ src/main/scala/snippets/Continuation.scala
@@ -0,0 +1,25 @@
+package snippets
+
+import cats.effect.kernel.Ref
+import cats.effect.{IO, IOApp}
+import fs2.Stream
+
+import scala.concurrent.duration.DurationInt
+
+object Continuation extends IOApp.Simple {
+
+  def increment(count: Ref[IO, Int], amount: Int): Stream[IO, Unit] =
+    Stream
+      .eval(count.updateAndGet(_ + amount))
+      .evalMap(c => IO(println(s"count = $c")))
+      .repeat
+      .metered(500.milliseconds)
+      .interruptAfter(5.seconds)
+
+  val program: Stream[IO, Unit] =
+    Stream.eval(Ref.of[IO, Int](0)).flatMap { count =>
+      increment(count, +1) ++ increment(count, -1)
+    }
+
+  override def run: IO[Unit] = program.compile.drain
+}
