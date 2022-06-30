05ac4593d49e4fe1155a6392290f30f43ea43a8b
Michael Nikolich <michael.nikolich@googlemail.com>
2022-06-30 14:45:28 +0100

    Improve the broadcasting example for a stream.

--- src/main/scala/snippets/Broadcasting.scala
+++ src/main/scala/snippets/Broadcasting.scala
@@ -1,24 +1,31 @@
 package snippets
 
-import cats.effect.{IO, IOApp}
+import cats.effect.{IO, IOApp, Ref}
 import fs2.{Pipe, Stream}
-
-import scala.concurrent.duration.DurationInt
 
 object Broadcasting extends IOApp.Simple {
 
-  def source: Stream[IO, Int] = Stream.emits(1 to 10).chunkLimit(1).unchunks
+  val source: Stream[IO, Int] = Stream.emits(1 to 10).chunkLimit(1).unchunks
 
-  val multiplier: Pipe[IO, Int, Int] = _.debug(i => s"I multiply $i by two and get ${i * 2.0}")
-  val divider: Pipe[IO, Int, Int] = _.debug(i => s"I divide $i by two and get ${i / 2.0}")
+  val program: Stream[IO, Int] =
+    Stream.eval(Ref.of[IO, Int](0)).flatMap { count =>
+      Stream.eval(Ref.of[IO, List[Int]](Nil)).flatMap { collection =>
 
-  override def run: IO[Unit] =
-    source
-      .broadcastThrough(
-        multiplier,
-        divider
-      )
-      .meteredStartImmediately(1000.millis)
-      .compile
-      .drain
+        val counter: Pipe[IO, Int, Int] = _.evalTap(_ => count.update(_ + 1))
+        val collector: Pipe[IO, Int, Int] = _.evalTap(i => collection.update(_ :+ i))
+
+        source
+          .broadcastThrough(
+            counter,
+            collector
+          )
+          .onFinalize {
+            count.get.flatMap(count => IO.println(s"Counted $count elements")) >>
+              collection.get.flatMap(collection => IO.println(s"Collected these elements: $collection"))
+          }
+      }
+    }
+
+  override def run: IO[Unit] = program.compile.drain
+
 }
