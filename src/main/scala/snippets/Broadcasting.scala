45d2bf80c36bcf2349f1098e4a0618e665beb13c
Michael Nikolich <michael.nikolich@googlemail.com>
2022-07-01 08:47:07 +0100

    Add an alternative broadcasting example that doesn't use shared concurrent state.

--- src/main/scala/snippets/Broadcasting.scala
+++ src/main/scala/snippets/Broadcasting.scala
@@ -7,14 +7,14 @@ object Broadcasting extends IOApp.Simple {
 
   val source: Stream[IO, Int] = Stream.emits(1 to 10)
 
-  val program: Stream[IO, Int] =
+  val program1: Stream[IO, Int] =
     Stream.eval(Ref.of[IO, Int](0)).flatMap { count =>
       Stream.eval(Ref.of[IO, List[Int]](Nil)).flatMap { collection =>
 
         val counter: Pipe[IO, Int, Int] = _.evalTap(_ => count.update(_ + 1))
         val collector: Pipe[IO, Int, Int] = _.evalTap(i => collection.update(_ :+ i))
 
-        source
+        Stream.exec(IO.println("Running program 1")) ++ source
           .broadcastThrough(
             counter,
             collector
@@ -26,6 +26,25 @@ object Broadcasting extends IOApp.Simple {
       }
     }
 
-  override def run: IO[Unit] = program.compile.drain
+  val program2: Stream[IO, Any] = {
+
+    val counter: Pipe[IO, Int, Int] =
+      _
+        .scan(0)((count, _) => count + 1)
+        .debug(count => s"Counted $count elements")
+
+    val collector: Pipe[IO, Int, List[Int]] =
+      _
+        .scan(List.empty[Int])((collection, i) => collection :+ i)
+        .debug(collection => s"Collected these elements: $collection")
+
+    Stream.exec(IO.println("Running program 2")) ++ source
+      .broadcastThrough(
+        counter,
+        collector
+      )
+  }
+
+  override def run: IO[Unit] = (program1 ++ program2).compile.drain
 
 }
