2e052de5b31e88d3681dc5de219154c2b0be4712
Michael Nikolich <michael.nikolich@googlemail.com>
2023-05-12 20:42:12 +0100

    Change the 'Pausing' example to use the new `Switch` abstraction.

--- src/main/scala/snippets/Pausing.scala
+++ src/main/scala/snippets/Pausing.scala
@@ -3,20 +3,21 @@ package snippets
 import cats.effect.{IO, IOApp}
 import fs2.Stream
 import fs2.concurrent.SignallingRef
+import syntax.*
 
 import scala.concurrent.duration.DurationInt
 
 object Pausing extends IOApp.Simple {
 
   private val program: Stream[IO, Unit] = {
-    Stream.eval(SignallingRef.of[IO, Boolean](true)).flatMap { signal =>
+    Stream.eval(Switch[IO](Switch.State.On)).flatMap { switch =>
       val timer = Stream.awakeEvery[IO](2.seconds).evalMap(d => IO(println(s"$d elapsed")))
-      val stuff = Stream.repeatEval(IO(println("I am doing stuff..."))).metered(1.second).pauseWhen(signal)
-      val switch = Stream.repeatEval(signal.update(!_) >> IO(println("switched!"))).metered(5.seconds)
+      val stuff = Stream.repeatEval(IO(println("I am doing stuff..."))).metered(1.second).pauseWhen(switch)
+      val flips = Stream.repeatEval(switch.flip >> IO(println("Switch flipped!"))).metered(5.seconds)
       Stream(
         timer,
         stuff,
-        switch
+        flips
       )
         .parJoinUnbounded
         .interruptAfter(30.seconds)
