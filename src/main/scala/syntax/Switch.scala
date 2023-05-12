f0b9b4463451b26cab9261f6bb1fecfa35bc63e5
Michael Nikolich <michael.nikolich@googlemail.com>
2023-05-12 20:40:36 +0100

    A 'switch' abstraction over `SignallingRef[F, Boolean]` using Scala 3's new opaque types.

---
+++ src/main/scala/syntax/Switch.scala
@@ -0,0 +1,24 @@
+package syntax
+
+import cats.Functor
+import cats.effect.Concurrent
+import cats.syntax.all.*
+import fs2.concurrent.{Signal, SignallingRef}
+
+opaque type Switch[F[_]] = SignallingRef[F, Boolean]
+
+object Switch:
+  enum State(val value: Boolean):
+    case On extends State(true)
+    case Off extends State(false)
+
+  def apply[F[_] : Concurrent](s: State): F[Switch[F]] = SignallingRef.of(s.value)
+
+extension[F[_] : Functor] (s: Switch[F])
+  def state: F[Switch.State] = s.get.map(s => if (s) Switch.State.On else Switch.State.Off)
+  def isOn: F[Boolean] = state.map(_ == Switch.State.On)
+  def isOff: F[Boolean] = state.map(_ == Switch.State.Off)
+  def switchOn: F[Unit] = s.set(true)
+  def switchOff: F[Unit] = s.set(false)
+  def flip: F[Unit] = s.update(!_)
+  implicit def asSignal: Signal[F, Boolean] = s
