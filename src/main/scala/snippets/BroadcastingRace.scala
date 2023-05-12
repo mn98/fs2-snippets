745d9435cfd4acecb3841394aa7dd82b4f41407e
Michael Nikolich <michael.nikolich@googlemail.com>
2023-05-12 09:13:43 +0100

    Use the recently added `Stream` extensions.

--- src/main/scala/snippets/BroadcastingRace.scala
+++ src/main/scala/snippets/BroadcastingRace.scala
@@ -2,30 +2,17 @@ package snippets
 
 import cats.effect.kernel.Ref
 import cats.effect.{IO, IOApp}
-import cats.syntax.all._
+import cats.syntax.all.*
 import fs2.{Pipe, Pull, Stream}
+import syntax.*
 
 object BroadcastingRace extends IOApp.Simple {
 
   type T = Int
 
-  def takeEveryNth[F[_], O](n: Int): Pipe[F, O, O] = in => {
-    def go(s: Stream[F, O]): Pull[F, O, Unit] =
-      s.pull.drop(n - 1).flatMap {
-        case Some(tl) =>
-          tl.pull.take(1).flatMap {
-            case Some(tl) => go(tl)
-            case None => Pull.done
-          }
-        case None => Pull.done
-      }
-
-    go(in).stream
-  }
-
   def source: Stream[IO, T] = Stream.emits(1 to 10000)
 
-  def writes(state: Ref[IO, T]): Pipe[IO, T, T] = _.through(takeEveryNth(42)).evalTap(state.set)
+  def writes(state: Ref[IO, T]): Pipe[IO, T, T] = _.takeEveryNth(42).evalTap(state.set)
 
   def reads(state: Ref[IO, T]): Pipe[IO, T, T] = _.evalTap { t =>
     state.get.flatMap { s =>
