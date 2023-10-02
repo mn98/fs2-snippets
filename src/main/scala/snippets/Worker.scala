7db998968ce7da4907614e8da1b75e2f139d1538
Michael Nikolich <michael.nikolich@googlemail.com>
2023-10-02 08:37:59 +0100

    A `Worker` that uses a `Channel` to have all work executed sequentially on the same fibre.

--- src/main/scala/snippets/Worker.scala
+++ src/main/scala/snippets/Worker.scala
@@ -2,9 +2,10 @@ package snippets
 
 import cats.effect.kernel.DeferredSource
 import cats.effect.std.{Queue, Semaphore, Supervisor}
-import cats.effect.{Concurrent, Deferred, IO, IOApp}
+import cats.effect.{Concurrent, Deferred, IO, IOApp, Resource}
 import cats.syntax.all.*
 import fs2.Stream
+import fs2.concurrent.Channel
 
 import scala.concurrent.duration.DurationInt
 
@@ -35,7 +36,7 @@ object Worker:
     Supervisor(false).use { supervisor =>
       Queue.dropping[F, Unit](max).map { q =>
         new Worker[F] {
-          def submit[A](task: F[A]): F[Option[DeferredSource[F, A]]] = {
+          def submit[A](task: F[A]): F[Option[DeferredSource[F, A]]] =
             q.tryOffer(()).flatMap { accepted =>
               if (accepted) {
                 Deferred[F, A].flatMap { result =>
@@ -45,7 +46,6 @@ object Worker:
                 None.pure
               }
             }
-          }
         }
       }
     }
@@ -61,12 +61,39 @@ object Worker:
     Supervisor(false).use { supervisor =>
       Semaphore[F](max).map { s =>
         new Worker[F] {
-          def submit[A](task: F[A]): F[Option[DeferredSource[F, A]]] = {
+          def submit[A](task: F[A]): F[Option[DeferredSource[F, A]]] =
             Deferred[F, A].flatMap { result =>
               supervisor.supervise(s.permit.surround(task.flatMap(result.complete))) *> Some(result).pure
             }
-          }
         }
+      }
+    }
+
+  /**
+   * A worker that executes all submitted tasks sequentially on the same fibre.
+   *
+   * @tparam F The effect type.
+   * @return A worker.
+   */
+  def channelling[F[_] : Concurrent]: Resource[F, Worker[F]] =
+    Supervisor(false).evalMap { supervisor =>
+      Channel.unbounded[F, F[Boolean]].flatMap { channel =>
+        val worker: Worker[F] = new Worker[F] {
+          def submit[A](task: F[A]): F[Option[DeferredSource[F, A]]] =
+            Deferred[F, A].flatMap { result =>
+              channel.send(task.flatMap(result.complete)) *> Some(result).pure
+            }
+        }
+        val work = supervisor.supervise(
+          channel
+            .stream
+            .map(Stream.eval)
+            .flatten
+            .debug(e => s"Channel emitted $e")
+            .compile
+            .drain
+        )
+        work *> worker.pure
       }
     }
 
@@ -83,16 +110,18 @@ object WorkerTest extends IOApp.Simple:
       tasks
         .zipWithIndex
         .metered(1.second)
-        .evalMap((task, i) => worker.submit(task).flatMap {
+        .evalMap((task, i) => IO.println(s"Submitting task $i") >> worker.submit(task).flatMap {
           case Some(result) =>
             IO.println(s"Accepted task $i") >>
               supervisor.supervise(result.get.flatMap(r => IO.println(s"Result of task $i: $r")))
           case None => IO.println(s"Rejected task $i")
         })
-        .interruptAfter(30.seconds)
+        .interruptAfter(21.seconds)
         .compile
         .drain
     }
 
   override def run: IO[Unit] =
-    Worker.dropping[IO](3).flatMap(program) >> Worker.queueing[IO](3).flatMap(program)
+  //    Worker.dropping[IO](3).flatMap(program) >>
+  //      Worker.queueing[IO](3).flatMap(program) >>
+    Worker.channelling[IO].use(program)
