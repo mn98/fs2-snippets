637189c4b40b31dc8b8b337610ae2e1a732a8d87
Michael Nikolich <michael.nikolich@googlemail.com>
2023-11-20 19:14:44 +0000

    Add AOC input files as project resources.

--- src/main/scala/aoc/AOCApp.scala
+++ src/main/scala/aoc/AOCApp.scala
@@ -4,19 +4,13 @@ import cats.effect.{IO, IOApp}
 import fs2.io.file.{Files, Flags, Path}
 import fs2.{Stream, text}
 
-import java.nio.file.Paths
-
 trait AOCApp extends IOApp.Simple {
 
   def inputFileName: String
 
   def input: Stream[IO, String] =
     Files[IO]
-      .readAll(
-        Path.fromNioPath(Paths.get(s"${System.getenv("HOME")}/Documents/$inputFileName")),
-        1024,
-        Flags.Read
-      )
+      .readAll(Path(getClass.getResource(s"/aoc/$inputFileName").getPath), 1024, Flags.Read)
       .through(text.utf8.decode andThen text.lines)
 
   def part1: Stream[IO, Unit]
